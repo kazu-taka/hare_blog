150c67b3904290c951cc3d2490227665e4c85a44
kazu-taka <k.takahashi@nowall.co.jp>
2023-11-05 09:57:45 +0900

    ブログのCRUD処理完成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -93,7 +93,7 @@ class PostController extends Controller
 
         $file = $request->file('image');
         if ($file) {
-            $delete_file_path = 'images/posts/' . $post->image;
+            $delete_file_path = $post->image_path;
             $post->image = self::createFileName($file);
         }
         $post->fill($request->all());
@@ -110,7 +110,7 @@ class PostController extends Controller
 
                 // 画像削除
                 if (!Storage::delete($delete_file_path)) {
-                    Storage::delete('images/posts/' . $post->image);
+                    Storage::delete($post->image_path);
                     throw new \Exception('画像ファイルの削除に失敗しました');
                 }
             }
@@ -137,7 +137,7 @@ class PostController extends Controller
         try {
             $post->delete();
 
-            if (!Storage::delete('images/posts/' . $post->image)) {
+            if (!Storage::delete($post->image_path)) {
                 throw new \Exception('画像ファイルの削除に失敗しました。');
             }
 
@@ -148,7 +148,7 @@ class PostController extends Controller
         }
 
         return redirect()->route('posts.index')
-            ->with('notice', '記事を作成しました');
+            ->with('notice', '記事を削除しました');
     }
 
     private static function createFileName($file)
